b671572e97c83a37b988ca01037156a365d2b4ec
Samith Dulanga <dulangasamith@gmail.com>
2023-08-15 20:26:03 +0530

    First Commit

---
+++ App_Assignment1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class App_Assignment1{
+    private static final Scanner SCANNER=new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
